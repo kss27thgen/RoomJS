e495a65f5e1fd60876d2c7c11f57580a3615092a
Oishi Yuikyo <s2008511@gmail.com>
2019-04-16 00:20:46 +0900

    Set root-path to vanillas#omikuji

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "vanillas/omikuji"
 
-  # root "home#links"
+  root "vanillas#omikuji"
 end
