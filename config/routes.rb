a338cc9bbf5dafae8c0dbbc058e3cffd199c3903
Oishi Yuikyo <s2008511@gmail.com>
2019-04-18 00:11:35 +0900

    Create password-generator app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "vanillas/omikuji"
-  get "vanillas/psw_generator"
+  get "vanillas/pwd_generator"
 
   get "home/top"
 
