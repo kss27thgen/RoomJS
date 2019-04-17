3052eb5796f61e98826684671ccff3c30b6bd7b8
Oishi Yuikyo <s2008511@gmail.com>
2019-04-17 21:39:38 +0900

    Setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "vanillas/omikuji"
+  get "vanillas/psw_generator"
 
   # root "home#links"
 end
